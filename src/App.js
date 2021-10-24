c2b29cede29d42671be5ebc48e7c05658a52b814
BikkyS <72158443+BikkyS@users.noreply.github.com>
2021-10-24 11:08:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+  <>
+    <Navbar title="Hello" abouttext="About TextUtils"/>
+    {/* <Navbar props="KYA"/> */}
+  </>
+  );
+}
+
+export default App;
